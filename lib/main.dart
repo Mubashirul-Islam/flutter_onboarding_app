f88c34adcf2c26f989704ca14d2994ab935c7e69
Md. Mubashirul Islam <md.mubashirul.islam@g.bracu.ac.bd>
2025-09-27 07:19:33 +0600

    Final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
+import 'constants/app_colors.dart';
+import 'constants/app_routes.dart';
+import 'constants/app_strings.dart';
+import 'helpers/app_pages.dart';
+import 'helpers/notification_helper.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NotificationHelper.initialize();
   runApp(const MainApp());
 }
 
@@ -9,12 +18,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: AppStrings.appTitle,
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.background,
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.accent),
+        useMaterial3: true,
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: AppColors.textPrimary,
+          displayColor: AppColors.textPrimary,
         ),
       ),
+      initialRoute: AppRoutes.onboarding,
+      getPages: AppPages.pages,
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_local_notifications
+import geolocator_apple
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
